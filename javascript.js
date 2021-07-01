610f59be615f934832b52b45c3ab944d0acdfe3a
fatima96-asy <fatmaaneis6@gmail.com>
2021-07-01 19:08:58 +0200

    responsive design

--- javascript.js
+++ javascript.js
@@ -4,5 +4,24 @@
     header.classList.toggle('sticky',scrollY>0);
   });
 
-// animate element when scroll
+//loader by vanella javaScript
+window.addEventListener('load',function(e){
+  var loader=document.getElementById('loader');
+  loader.classList.add('hidden');
+  
+})
+// arrow up 
+window.addEventListener('scroll',function(e){
+  var arrow=document.querySelector('#arrow');  
+  arrow.classList.toggle('hidden',scrollY<0);
+});
+
+var arrow=document.querySelector('#arrow'); 
+arrow.addEventListener('click',function(e){
+  var rootElement = document.documentElement
+  rootElement.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  })
+}) 
 
